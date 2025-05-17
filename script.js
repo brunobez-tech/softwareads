9d29bd26157f66c98234de5d23d7a95d4357c90e
brunobez-tech <bruno.bez.alm@gmail.com>
2025-05-17 15:02:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+//função botão
+function mostrarMensagem() {
+    document.getElementById("mensagem").textContent = "A competitividade de um país não começa nas indústrias ou nos laboratórios de engenharia. Ela começa na sala de aula. (Lee Iacocca)" ;
+}
